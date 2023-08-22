3639371b2d7c6885e9b636b1efded2c6c34f3fc9
Alana Darcher <alana.darcher@gmail.com>
2023-08-22 11:47:33 +0000

    changing subheader

--- round1.py
+++ round1.py
@@ -16,7 +16,7 @@ import streamlit as st                  # https://docs.streamlit.io/library/api-
 import pandas as pd                     # https://pandas.pydata.org/docs/reference/index.html
 
 st.title('Middle school clss trip')
-st.subheader('Subheader') 
+st.subheader('funny coding game') 
 st.text('Text')
 st.write('## Subheader')
 df = pd.read_csv('data/round1.csv') 
