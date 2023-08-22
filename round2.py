1505726c9b842084296229d21ef60942866458ff
Moritz Nesseler <m.nesseler@bio2.rwth-aachen.de>
2023-08-22 13:31:20 +0000

    hahaha

--- round2.py
+++ round2.py
@@ -67,7 +67,7 @@ st.write(fig)
 
 fig,ax=plt.subplots(1,1)
 ax.scatter(df['Trial'],df['Time'])
-ax.set_xlabel('Trial')
+ax.set_xlabel('Trial')##
 ax.set_ylabel('Time') ###
 st.write(fig) #writes figure to board
 
