6151bb9b6b26860a2ef2406da37438e8b3d4d5fe
haddock99 guha <sguha@uni-bonn.de>
2023-08-22 12:03:47 +0000

    some msg

--- round1.py
+++ round1.py
@@ -16,7 +16,7 @@ import streamlit as st                  # https://docs.streamlit.io/library/api-
 import pandas as pd                     # https://pandas.pydata.org/docs/reference/index.html
 
 st.title('Neuron')
-st.subheader('Subheader') 
+st.subheader('pyramidal neuron') 
 st.text('Text')
 st.write('## Subheader')
 df = pd.read_csv('data/round1.csv') 
