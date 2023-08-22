a103fdc8ef57a30d0fd69e4087d6afceee4550d8
Uttam Kumar <uttam1216@gmail.com>
2023-08-22 11:44:57 +0000

    added title

--- round1.py
+++ round1.py
@@ -15,8 +15,9 @@ df['column_A'].unique()
 import streamlit as st                  # https://docs.streamlit.io/library/api-reference
 import pandas as pd                     # https://pandas.pydata.org/docs/reference/index.html
 
-st.title('Title')
+st.title('Round 1 Data Dashboard')
 st.subheader('Subheader') 
 st.text('Text')
 st.write('## Subheader')
 df = pd.read_csv('data/round1.csv') 
+#print(df)
