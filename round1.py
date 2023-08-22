143b7d0c8bee3aaea1d01d3fcc64c8352834d82a
Alana Darcher <alana.darcher@gmail.com>
2023-08-22 11:44:21 +0000

    space in title

--- round1.py
+++ round1.py
@@ -15,7 +15,7 @@ df['column_A'].unique()
 import streamlit as st                  # https://docs.streamlit.io/library/api-reference
 import pandas as pd                     # https://pandas.pydata.org/docs/reference/index.html
 
-st.title('Title')
+st.title('Title ')
 st.subheader('Subheader') 
 st.text('Text')
 st.write('## Subheader')
